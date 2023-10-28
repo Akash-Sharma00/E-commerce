bbe2f1e816c4ac0e0fd5d7c7aa7da436eb400a95
Akash <akashvsh13@gmail.com>
2023-10-28 12:35:46 +0530

    final ui push

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -5,48 +5,51 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
-  for (let index = 0; index < all_product.length+1; index++) {
+  for (let index = 0; index < all_product.length + 1; index++) {
     cart[index] = 0;
   }
   return cart;
 };
 
 const ShopContextProvider = (props) => {
-  
-  const [cartItem,setCartItem] = useState(getDefaultCart());
-  
-  
-  const addToCart = (id)=>{
-    setCartItem((prev)=>({...prev,[id]:prev[id]+1}))
-  }
-  const removeFromCart = (id)=>{
-    setCartItem((prev)=>({...prev,[id]:prev[id]-1}))
-  }
+  const [cartItem, setCartItem] = useState(getDefaultCart());
 
-  const getTotalCartAmount = ()=>{
+  const addToCart = (id) => {
+    setCartItem((prev) => ({ ...prev, [id]: prev[id] + 1 }));
+  };
+  const removeFromCart = (id) => {
+    setCartItem((prev) => ({ ...prev, [id]: prev[id] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
     let amount = 0;
-    for(const item in cartItem){
-      if(cartItem[item]>0){
-        console.log(all_product.find((e)=>e.id === Number(item)))
-        let itemInfo = all_product.find((e)=>e.id === Number(item));
-        amount += itemInfo.new_price * cartItem[item]; 
+    for (const item in cartItem) {
+      if (cartItem[item] > 0) {
+        console.log(all_product.find((e) => e.id === Number(item)));
+        let itemInfo = all_product.find((e) => e.id === Number(item));
+        amount += itemInfo.new_price * cartItem[item];
       }
     }
     return amount;
-  }
+  };
 
-  const getTotalItem = ()=>{
+  const getTotalItem = () => {
     let totalItem = 0;
-    for(const item in cartItem){
-      {
-        if(cartItem[item]>0){
-          totalItem += cartItem[item];
-        }
+    for (const item in cartItem) {
+      if (cartItem[item] > 0) {
+        totalItem += cartItem[item];
       }
     }
     return totalItem;
-  }
-    const contextValues = { all_product,cartItem,addToCart,removeFromCart,getTotalCartAmount,getTotalItem };
+  };
+  const contextValues = {
+    all_product,
+    cartItem,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+    getTotalItem,
+  };
 
   return (
     <ShopContext.Provider value={contextValues}>
